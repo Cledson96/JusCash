3c269d663545456b65955ef1331759caa5d15c57
cledson96 <cledson1996@gmail.com>
2024-08-15 21:24:24 -0300

    feature: add toastify

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,30 @@ import Teste from "./views/Test";
 import Auth from "./views/Auth";
 import "./styles/reset.css";
 import "./styles/style.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/teste" element={<Teste />} />
-        <Route path="/" element={<Auth />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/teste" element={<Teste />} />
+          <Route path="/" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
